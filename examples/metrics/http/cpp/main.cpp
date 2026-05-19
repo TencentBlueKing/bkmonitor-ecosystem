f2acab3996753ad570f6d2d7562fa4733d501300
joker-joker-yuan <blackpig163@163.com>
2026-05-19 11:26:05 +0800

    fix: 【样例仓库】代码规范问题修复 #1010158081158166440 # Reviewed, transaction id: 80484

--- examples/metrics/http/cpp/main.cpp
+++ examples/metrics/http/cpp/main.cpp
@@ -6,10 +6,10 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-#include <cstdlib>
 #include <cpr/cpr.h>
 
 #include <chrono>
+#include <cstdlib>
 #include <iomanip>
 #include <iostream>
 #include <random>
